392b284a9112486e0bb78e62b09d236422b64276
Dmytro Holub <dmitriygolubapk@gmail.com>
2024-01-20 12:28:29 +0300

    Root page: open modal  onClick report button between 1st & 2nd sections

--- src/view/pages/Root/index.tsx
+++ src/view/pages/Root/index.tsx
@@ -1,5 +1,5 @@
 // Core
-import React, { FC } from 'react';
+import React, { FC, useContext } from 'react';
 
 // Components
 import { ErrorBoundary, FifthBlock, FirstBlock, FourthBlock, SecondBlock, ThirdBlock } from '@/view/components';
@@ -7,8 +7,11 @@ import { ErrorBoundary, FifthBlock, FirstBlock, FourthBlock, SecondBlock, ThirdB
 // Styles
 import * as S from './styles';
 import { Button } from '@/view/elements';
+import { ModalLandingContext } from '@/layouts';
 
 const Root: FC = () => {
+    const context = useContext(ModalLandingContext);
+
     return (
         <S.Container>
             <FirstBlock />
@@ -17,7 +20,8 @@ const Root: FC = () => {
                     <S.Text>Keep the web safe – report suspicious activity</S.Text>
                     <Button
                         $styles = { S.Button }
-                        $variant = 'white_outlined'>Report a Scam
+                        $variant = 'white_outlined'
+                        onClick = { () => context?.setModalActive(!context.isModalActive) }>Report a Scam
                     </Button>
                 </S.BottomTextWrapper>
             </S.Bottom>
